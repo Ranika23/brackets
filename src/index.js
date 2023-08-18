e01d41afd5b8a4e3b917582c0d9bcdb5188add70
Ranika23 <bveronikal@gmail.com>
2023-08-18 16:02:45 +0300

    docs: add function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {
+    ')':'(',
+    ']':'[',
+    '}':'{',
+    '|':'|',
+    '2':'1',
+    '4':'3',
+    '6':'5',
+    '7':'7',
+    '8':'8'
+}
+  let arr = []
+  arr.push(...str)
+  let i = 0
+  let count= 0
+  while (arr.length > 0) {
+         let current = arr[i]
+         let second = arr[i + 1]
+         if (current != undefined && second != undefined) {
+         if (current == brackets[second]) {
+            arr.splice(i, 2)
+            i = 0
+         }
+         else {
+            i++
+         }
+         count += 1
+         }
+         else { break}
+     }
+  return arr.length == 0
 }
